9aa378ec5a928e37e4984f93613f9059dad9d238
Owen Herterich <oherterich@gmail.com>
2014-03-11 21:42:59 -0400

    added leader functionality

--- taptap/js/nodeClient.js
+++ taptap/js/nodeClient.js
@@ -1,13 +1,29 @@
 var socket = io.connect( 'http://localhost:8080' );
 
+var main = document.getElementById("main");
+var controls = document.getElementById("controls");
+
+var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+main.style.background = color;
+
+var turnOnControls = function() {
+	controls.classList.remove('controls-hidden');
+	controls.classList.add('controls-visible');
+}
+
 main.addEventListener("click", function(evt) {
 	socket.emit( 'click', { message: "I just clicked!" });
 });
 
-socket.on('init', function( data ) {
+socket.on('init', function ( data ) {
 	console.log( data.message );
 });
 
 socket.on('new user', function ( data ) {
 	console.log("New user with id: " + data.id);
+});
+
+socket.on('controls', function ( data ) {
+	turnOnControls();
+	console.log( data.message );
 });
--- taptap/js/nodeServer.js
+++ taptap/js/nodeServer.js
@@ -9,14 +9,19 @@ var io = socket.listen( server );
 
 var players = [];
 
-var Player = function( id, score ) {
+var Player = function( client, id, score ) {
+	this.client = client;
 	this.id = id;
 	this.score = score;
 }
 
 io.sockets.on( 'connection', function( client ) {
 
-	players.push( new Player( client.id, 0 ) );
+	if ( players.length < 1 ) {
+		client.emit( 'controls', { message: "You are the leader!" } );
+	}
+
+	players.push( new Player( client, client.id, 0 ) );
 
 	client.broadcast.emit( 'new user', { id: client.id });
 
@@ -30,6 +35,16 @@ io.sockets.on( 'connection', function( client ) {
 			console.log( players[i].id + " | " + players[i].score );
 		}
 	});
+
+	client.on('disconnect', function (data) {
+		console.log(client.id + " has disconnected!");
+		for ( var i = 0; i < players.length; i++ ) {
+			if ( players[i].id == client.id ) {
+				players.splice(i, 1);
+				players[0].client.emit('controls', { message: "You are now the leader!" } ); 
+			}
+		}
+	});	
 });
 
 server.listen( 8080 );
